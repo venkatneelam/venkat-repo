1eba404c8f88d22c81f442145377b0ac192d816f
venkat <venkat@gmail.com>
2021-09-29 17:15:17 +0530

    second commit

---
+++ arraylistdemo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+class arraylistdemo1{
+public static void main (String args[])
+{
+// declary Array List
+ArrayList a1=new ArrayList();
+a1.add(100);
+a1.add("venkat");
+a1.add(10.5);
+a1.add("revathi");
+{
+System.out.println("a1");
+}
+}
+}
